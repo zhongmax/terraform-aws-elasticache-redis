9d4cd3212de174eedeb2877fa5056d8545cc77d6
maxwell <zhong.maxwell@gmail.com>
2023-08-02 14:45:28 +0800

    add var parameter_group_name

--- main.tf
+++ main.tf
@@ -88,9 +88,9 @@ resource "aws_elasticache_subnet_group" "default" {
   tags        = module.this.tags
 }
 
-resource "aws_elasticache_parameter_group" "default" {
+resource "aws_elasticache_parameter_group" "this" {
   count       = module.this.enabled ? 1 : 0
-  name        = module.this.id
+  name = var.parameter_group_name != null ? var.parameter_group_name : module.this.id
   description = var.parameter_group_description != null ? var.parameter_group_description : "Elasticache parameter group for ${module.this.id}"
   family      = var.family
 
